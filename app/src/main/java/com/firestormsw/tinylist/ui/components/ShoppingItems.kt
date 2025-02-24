fa0a592e3d7e494666710a12e3300bb6414ce9cf
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2025-02-24 20:49:47 +0200

    Added a label indicating when all items on the list have been checked off

--- app/src/main/java/com/firestormsw/tinylist/ui/components/ShoppingItems.kt
+++ app/src/main/java/com/firestormsw/tinylist/ui/components/ShoppingItems.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,6 +17,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -70,6 +73,17 @@ fun ShoppingItems(
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(6.dp),
     ) {
+        if (uncheckedItems.isEmpty() && checkedItems.isNotEmpty()) {
+            item(key = "all-checked-header") {
+                Box {
+                    Text(
+                        stringResource(R.string.all_items_have_been_checked_off),
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+                }
+            }
+        }
+
         items(
             items = uncheckedItems,
         ) { item ->
