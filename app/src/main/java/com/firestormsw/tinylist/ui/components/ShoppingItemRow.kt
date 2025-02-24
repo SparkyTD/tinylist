f9b2e7b17f15dec703d8e2f04f11ff83a3f50eba
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2025-02-24 16:27:19 +0200

    Fixed interaction bug with list item checking / unchecking

--- app/src/main/java/com/firestormsw/tinylist/ui/components/ShoppingItemRow.kt
+++ app/src/main/java/com/firestormsw/tinylist/ui/components/ShoppingItemRow.kt
@@ -1,5 +1,9 @@
 package com.firestormsw.tinylist.ui.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -27,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalHapticFeedback
@@ -37,6 +42,7 @@ import androidx.compose.ui.unit.dp
 import com.firestormsw.tinylist.R
 import com.firestormsw.tinylist.data.ShoppingItem
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ShoppingItemRow(
     item: ShoppingItem,
@@ -67,19 +73,22 @@ fun ShoppingItemRow(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(horizontal = 12.dp)
-                    .pointerInput(Unit) {
-                        detectTapGestures(onLongPress = { offset ->
+                    .combinedClickable(
+                        interactionSource = null,
+                        indication = null,
+                        onClick = {
+                            onItemClick(item.id)
+                        },
+                        onLongClick = {
                             haptic.performHapticFeedback(HapticFeedbackType.LongPress)
 
                             menuOffset = DpOffset(
-                                offset.x.toDp(),
-                                offset.y.toDp()
+                                100.dp,// offset.x.toDp(),
+                                0.dp,// offset.y.toDp()
                             )
                             showMenu = true
-                        }, onTap = {
-                            onItemClick(item.id)
-                        })
-                    },
+                        },
+                    ),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 AnimatedCheckbox(item.isChecked) {
